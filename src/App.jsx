26beb4c2006ea295b8d6bbfe639ed6d7d7a25c58
Rioedev <lequy11082003@gmail.com>
2023-05-29 11:30:01 +0700

    thiết lập routes cho dự án

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { Fragment } from "react";
-// import { NavLink } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "swiper/scss";
-import MovieList from "./components/movie/MovieList";
+import HomePage from "./pages/HomePage";
+import Main from "./components/layout/Main";
 import Banner from "./components/banner/Banner";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <Fragment>
-      <header className="flex items-center justify-center py-10 mb-5 text-white header gap-x-5">
-        <span className="text-primary">Home</span>
-        <span>Movies</span>
-      </header>
-      <Banner></Banner>
-      <section className="pb-10 movies-layout page-container">
-        <h2 className="mb-10 text-3xl font-bold text-white capitalize">
-          Now playing
-        </h2>
-        <MovieList></MovieList>
-      </section>
-      <section className="pb-10 movies-layout page-container">
-        <h2 className="mb-10 text-3xl font-bold text-white capitalize">
-          Top Rated
-        </h2>
-        <MovieList type="top_rated"></MovieList>
-      </section>
-      <section className="pb-10 movies-layout page-container">
-        <h2 className="mb-10 text-3xl font-bold text-white capitalize">
-          Trending
-        </h2>
-        <MovieList type="popular"></MovieList>
-      </section>
+      <Routes>
+        <Route element={<Main></Main>}>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner></Banner>
+                <HomePage></HomePage>
+              </>
+            }
+          ></Route>
+          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+        </Route>
+      </Routes>
     </Fragment>
   );
 }
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,11 @@
+const MoviePage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum voluptas
+      mollitia aperiam nostrum nisi impedit maiores id voluptatem quas, sed
+      saepe est autem totam eius. Labore quos ut omnis eveniet.
+    </div>
+  );
+};
+
+export default MoviePage;
