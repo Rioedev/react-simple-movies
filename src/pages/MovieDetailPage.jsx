7f62d29d6bcea0ac13094ac168a9c5d5de1ac223
Rioedev <lequy11082003@gmail.com>
2023-05-30 14:36:10 +0700

    trang chi tiết - fetching dữ liệu diễn viên

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import { apiKey, fetcher } from "../config";
 
 // https://api.themoviedb.org/3/movie/movie_id?
+// https://api.themoviedb.org/3/movie/movie_id/credits?
 
 const MovieDetailPage = () => {
   const { movieId } = useParams();
@@ -31,7 +32,7 @@ const MovieDetailPage = () => {
           className="object-cover w-full h-full rounded-lg"
         />
       </div>
-      <h1 className="mb-10 text-3xl font-bold text-center text-white">
+      <h1 className="mb-10 text-4xl font-bold text-center text-white">
         {title}
       </h1>
       {genres.length > 0 && (
@@ -46,11 +47,53 @@ const MovieDetailPage = () => {
           ))}
         </div>
       )}
-      <p className="mx-auto leading-relaxed max-w-[800px] text-center">
+      <p className="mx-auto leading-relaxed max-w-[800px] text-center mb-10">
         {overview}
       </p>
+      <MovieCredits></MovieCredits>
+      <MovieVideos></MovieVideos>
     </div>
   );
 };
+
+function MovieCredits() {
+  // https://api.themoviedb.org/3/movie/movie_id/credits?
+  const { movieId } = useParams();
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  const { cast } = data;
+  if (!cast || cast.length <= 0) return null;
+  return (
+    <>
+      <h2 className="mb-10 text-3xl text-center">Casts</h2>
+      <div className="grid grid-cols-4 gap-5 px-52">
+        {cast.slice(0, 8).map((item) => (
+          <div className="cast-item" key={item.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${item.profile_path}`}
+              className="w-full h-[450px] object-cover rounded-lg mb-3"
+              alt=""
+            />
+            <h3 className="text-xl font-medium">{item.name}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function MovieVideos() {
+  const { movieId } = useParams();
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  return <div></div>;
+}
+// <iframe width="893" height="502" src="https://www.youtube.com/embed/9nv0n-Wwwkk" title="10 Mac Tips I Regret Not Using Sooner" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 export default MovieDetailPage;
