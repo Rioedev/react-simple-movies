9a22ba9552b2b99d08c90e5cfd848b88bb8e86d8
Rioedev <lequy11082003@gmail.com>
2023-05-31 22:40:07 +0700

    loading

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -33,7 +33,7 @@ function BannerItem({ item }) {
         alt=""
         className="object-contain w-full h-full rounded-lg"
       />
-      <div className="absolute w-full text-white bottom-5 left-5">
+      <div className="absolute w-full text-white bottom-10 left-10">
         <h2 className="mb-5 text-3xl font-bold">{title}</h2>
         <div className="flex items-center mb-8 gap-x-3">
           <span className="px-4 py-2 border border-white rounded-md">
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -15,7 +15,8 @@ const MoviePage = () => {
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
-  const { data } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher);
+  const loading = !data && !error;
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
@@ -54,11 +55,55 @@ const MoviePage = () => {
           </svg>
         </button>
       </div>
+      {loading && (
+        <div className="w-10 h-10 mx-auto border-4 border-t-4 rounded-full border-primary border-t-transparent animate-spin"></div>
+      )}
       <div className="grid grid-cols-4 gap-10">
-        {movies.length > 0 &&
+        {!loading &&
+          movies.length > 0 &&
           movies.map((item) => (
             <MovieCard key={item.id} item={item}></MovieCard>
           ))}
+      </div>
+      <div className="flex items-center justify-center mt-10 gap-x-5">
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </span>
+        <span className="inline-block px-4 py-2 leading-none bg-white rounded-md cursor-pointer text-slate-900">
+          1
+        </span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </span>
       </div>
     </div>
   );
