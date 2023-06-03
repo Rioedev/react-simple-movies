ac52c30c77f72d1bf5c6cb4d8e9e3707df4b457a
Rioedev <lequy11082003@gmail.com>
2023-06-03 23:32:28 +0700

    refactor ui not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,25 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 const NotFoundPage = () => {
   return (
-    <div className="bg-indigo-900 relative overflow-hidden h-screen">
-      {" "}
-      <img
-        src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
-        className="absolute h-full w-full object-cover"
-      />
-      <div className="inset-0 bg-black opacity-25 absolute"></div>{" "}
-      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-        {" "}
-        <div className="w-full font-mono flex flex-col items-center relative z-10">
-          {" "}
-          <h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
-            You are all alone here{" "}
-          </h1>{" "}
-          <p className="font-extrabold text-8xl my-44 text-white animate-bounce">
-            404{" "}
-          </p>{" "}
-        </div>{" "}
+    <main className="h-screen w-full flex flex-col justify-center items-center bg-slate-900">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        404
+      </h1>
+      <div className="bg-primary px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
       </div>
-    </div>
+      <button className="mt-5">
+        <a className="relative inline-block text-sm font-medium text-primary group active:text-orange-500 focus:outline-none focus:ring">
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-primary group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+            <NavLink to={"/"}>Go Home</NavLink>
+          </span>
+        </a>
+      </button>
+    </main>
   );
 };
 
